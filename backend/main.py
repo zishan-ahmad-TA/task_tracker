2a1b1ec641d3a16d16db3ac0909247c4f1f43d86
Zishan Ahmad <zishan.ahmad@tigeranalytics.local>
2024-11-23 14:36:35 +0530

    added a direct startup

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 import uvicorn
+import sys
+import os
 
 if __name__ == "__main__":
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'app'))
+
     uvicorn.run(
-        app = "app.main:app",
-        host = "localhost",
-        port = 8000,
+        app="app.main:app",
+        host="localhost",
+        port=8000,
         reload=True
     )
