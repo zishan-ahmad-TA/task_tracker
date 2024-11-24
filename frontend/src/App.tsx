98469bfd0de68c465cbb3efc18a420d7afef8d1a
Zishan Ahmad <zishan.ahmad@tigeranalytics.local>
2024-11-24 14:19:25 +0530

    added a empty login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
-function App() {
-  return <div>Hello</div>;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/shared/Login";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ frontend/src/components/shared/Login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div> Hello</div>;
+};
+
+export default Login;
