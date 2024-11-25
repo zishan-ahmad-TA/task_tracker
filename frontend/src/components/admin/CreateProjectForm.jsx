53aefb58ce792c5fe7aac7521b1e5f787cd3b66f
Zishan Ahmad <zishan.ahmad@tigeranalytics.local>
2024-11-26 01:19:58 +0530

    added date picker

--- frontend/src/components/admin/CreateProjectForm.jsx
+++ frontend/src/components/admin/CreateProjectForm.jsx
@@ -1,4 +1,6 @@
 import Input from "../ui-elements/Input";
+import { DatePicker } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
 import MultiSelect from "../ui-elements/MultiSelect";
 import styles from './CreateProjectForm.module.css';
 
@@ -17,6 +19,33 @@ const CreateProjectForm = ({ members, managers }) => {
         <div className={styles.CreateForm}>
             <Input label="Project Name" id="project_name" />
             <Input label="Project Description" id="project_description" />
+
+            <fieldset className={styles.Fieldset}>
+                <label className={styles.Label} htmlFor="start_date">
+                    Start Date
+                </label>
+                <DatePicker
+                    oneTap
+                    placeholder="Select Date"
+                    style={{ width: '70%' }}
+                    className={styles.DatePicker}
+                />
+
+            </fieldset>
+
+            <fieldset className={styles.Fieldset}>
+                <label className={styles.Label} htmlFor="end_date">
+                    End Date
+                </label>
+                <DatePicker
+                    oneTap
+                    placeholder="Select Date"
+                    style={{ width: '70%' }}
+                    className={styles.DatePicker}
+                />
+
+            </fieldset>
+
             <fieldset className={styles.Fieldset}>
                 <label className={styles.Label} htmlFor="managers">
                     Managers
@@ -34,3 +63,4 @@ const CreateProjectForm = ({ members, managers }) => {
 }
 
 export default CreateProjectForm;
+
