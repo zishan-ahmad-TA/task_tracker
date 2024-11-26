cd8f7f515035f7c208a625efb67ebc28c6c2f3a5
Zishan Ahmad <zishan.ahmad@tigeranalytics.local>
2024-11-27 00:45:20 +0530

    toast changes

--- frontend/src/components/ui-elements/Toast.jsx
+++ frontend/src/components/ui-elements/Toast.jsx
@@ -2,39 +2,39 @@ import * as Toast from "@radix-ui/react-toast";
 import styles from "./Toast.module.css";
 
 const ToastComponent = ({
-  open,
-  setOpen,
-  toastTitle = "Notification",
-  toastMessage = "Something happened!",
-  buttonActionText = null,
-  onActionClick = null,
+    open,
+    setOpen,
+    toastTitle = "Notification",
+    toastMessage = "Something happened!",
+    buttonActionText = 'Close',
+    onActionClick = () => { },
 }) => {
-  return (
-    <Toast.Provider swipeDirection="right">
-      <Toast.Root className={styles.Root} open={open} onOpenChange={setOpen}>
-        <Toast.Title className={styles.Title}>{toastTitle}</Toast.Title>
-        <Toast.Description className={styles.Description}>
-          {toastMessage}
-        </Toast.Description>
+    return (
+        <Toast.Provider swipeDirection="right">
+            <Toast.Root className={styles.Root} open={open} onOpenChange={setOpen}>
+                <Toast.Title className={styles.Title}>{toastTitle}</Toast.Title>
+                <Toast.Description className={styles.Description}>
+                    {toastMessage}
+                </Toast.Description>
 
-        {buttonActionText && onActionClick && (
-          <Toast.Action
-            className={styles.Action}
-            asChild
-            altText="Action button"
-          >
-            <button
-              className={`${styles.Button} small green`}
-              onClick={onActionClick}
-            >
-              {buttonActionText}
-            </button>
-          </Toast.Action>
-        )}
-      </Toast.Root>
-      <Toast.Viewport className={styles.Viewport} />
-    </Toast.Provider>
-  );
+                {buttonActionText && onActionClick && (
+                    <Toast.Action
+                        className={styles.Action}
+                        asChild
+                        altText="Action button"
+                    >
+                        <button
+                            className={`${styles.Button} small green`}
+                            onClick={onActionClick}
+                        >
+                            {buttonActionText}
+                        </button>
+                    </Toast.Action>
+                )}
+            </Toast.Root>
+            <Toast.Viewport className={styles.Viewport} />
+        </Toast.Provider>
+    );
 };
 
 export default ToastComponent;
